0fd097b44d8aa75316e73476798962f9e8b1b379
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-05-08 08:50:20 -0400

    Prelim file set up, pseudocode, basic functions written

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+//make a calculator
+
+//functions for each math operator
+//add, subtract, multiply, divide
+
+function addition (a, b) {
+    return a +b;
+}
+
+function subtration (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+//variables for each part of equation
+//first number, operand, second number
+
+//function called "operate" that takes operator and 2 numbers
+//uses math functions above
+
+//need html side of calc
+//buttons, display, equals key, clear key
+
+//Create the functions that populate the display when you click the number buttons. 
+//You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
