ed43c7efe136e6ffacb032dc919e64e5c1eaa89b
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-05-09 19:42:55 -0400

    created basic functions for assigning numbers from buttons on UI and the logic to maintain two digits

--- scripts.js
+++ scripts.js
@@ -25,11 +25,13 @@ function divide (a, b) {
 let numOne;
 let NumTwo;
 let operator;
+let opChoice = false;
+let display = document.querySelector(".display-text");
 
-let one = document.querySelector("one");
-let two = document.querySelector("two");
-let three = document.querySelector("three");
-let plus = document.querySelector("plus");
+const one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let add = document.querySelector(".add");
 
 
 //function called "operate" that takes operator and 2 numbers
@@ -42,6 +44,45 @@ function operate (a, b, oper) {
 //Create the functions that populate the display when you click the number buttons. 
 //You should be storing the ‘display value’ in a variable somewhere for use in the next step.
 
-one,addEventListener("click", () => {
-    
+one.addEventListener('click', () => {
+    display.textContent = "1";
+    if (numOne === undefined) {
+        numOne = 1;
+        console.log("numOne " + numOne);
+    }
+
+    else if (NumTwo === undefined && opChoice === false){
+        NumOne = 1;
+        console.log("numOne " + NumOne);
+    }
+
+    else if (NumTwo === undefined && opChoice === true) {
+        NumTwo = 1;
+        console.log("NumTwo " + NumTwo) 
+    }
+});
+
+two.addEventListener('click', () => {
+    display.textContent = "1";
+    if (numOne === undefined) {
+        numOne = 2;
+        console.log("numOne " + numOne);
+    }
+
+    else if (NumTwo === undefined && opChoice === false){
+        NumOne = 2;
+        console.log("numOne " + NumOne);
+    }
+
+    else if (NumTwo === undefined && opChoice === true) {
+        NumTwo = 2;
+        console.log("NumTwo " + NumTwo) 
+    }
+});
+
+add.addEventListener('click', () => {
+    opChoice = true;
 })
+
+console.log(numOne);
+console.log(NumTwo);
